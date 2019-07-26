462c314c3b5ff4148ebead523b93b27f311a2d5a
Andrew Ting <50852725+andrewting112@users.noreply.github.com>
2019-07-26 09:28:11 -0700

    Add files via upload

---
+++ w5-recursions/sum-from-a-to-b-test-js/index.js
@@ -0,0 +1,14 @@
+
+
+function sum(fromN, toN) {
+  if(fromN === toN) {
+    return toN;
+  } else {
+    return fromN + sum(fromN + 1, toN);
+  }
+};
+
+
+console.log(sum(3, 7));
+
+module.exports = sum;
